0013c90c2ac55a008a727be875216e1477bf504f
苍海之南 <1782920040@qq.com>
2019-12-13 10:37:06 +0800

    Fix：管理员新增待审核界面

--- src/pages/share/list/index.vue
+++ src/pages/share/list/index.vue
@@ -30,13 +30,24 @@
           <h3>公众号(java泡泡糖)</h3>
         </div>
       </van-tab>
+
+      <van-tab title="待审核" v-if="showFlag">
+        <van-notice-bar :text="notice.content"></van-notice-bar>
+        <van-search placeholder="请输入关键词" @change="onChange"/>
+        <ul>
+          <li v-for="(share_c,index) in shares_contribution" :key="index">
+            <Card :share="share_c" :key="index"></Card>
+          </li>
+        </ul>
+      </van-tab>
     </van-tabs>
   </div>
 </template>
 <script>
   import Card from '@/components/card.vue';
   import {request} from "@/utils/request";
-  import {BASE_API_URL, NOTICE_URL, SHARE_POST_URL} from "@/utils/api";
+  import {NOTICE_URL, SHARE_POST_URL} from "@/utils/api";
+  import {USER_URL_ME} from "../../../utils/api";
 
   export default {
     components: {
@@ -49,7 +60,9 @@
         more: true,
         pageNo: 1,
         searchWord: '',
-        title: ''
+        title: '',
+        shares_contribution: '',
+        showFlag: false
       }
     },
     methods: {
@@ -63,6 +76,18 @@
           'GET',
           {}
         );
+      },
+      async getUser() {
+        let user = await request(
+          USER_URL_ME,
+          'GET',
+          {}
+        );
+
+        if (user.roles === 'admin') {
+          console.log("当前用户权限：" + user.roles)
+          this.showFlag = true;
+        }
       },
       async fetchSharePosts(init) {
         if (init) {
@@ -89,6 +114,36 @@
         } else {
           // 下拉刷新，不能直接覆盖books 而是累加
           this.shares = this.shares.concat(sharesRemote.list)
+        }
+        wx.hideNavigationBarLoading();
+      },
+
+      async fetchSharePosts_a(init) {
+        if (init) {
+          this.pageNo = 1;
+          this.more = true;
+        }
+        wx.showNavigationBarLoading();
+        let title = this.title;
+        const sharesRemote = await request(
+          SHARE_POST_URL + '/q',
+          'GET',
+          {
+            pageNo: this.pageNo,
+            title: title,
+            type: 'NOT_YET'
+          }
+        );
+        if (sharesRemote.list.length < 10 && this.pageNo > 0) {
+          this.more = false;
+          console.log(this.more)
+        }
+        if (init) {
+          this.shares_contribution = sharesRemote.list;
+          wx.stopPullDownRefresh();
+        } else {
+          // 下拉刷新，不能直接覆盖books 而是累加
+          this.shares_contribution = this.shares_contribution.concat(sharesRemote.list)
         }
         wx.hideNavigationBarLoading();
       },
@@ -122,17 +177,21 @@
       if (!this.more) {
         return false
       }
-      this.pageNo = this.pageNo + 1
-      this.fetchSharePosts()
+      this.pageNo = this.pageNo + 1;
+      this.fetchSharePosts();
+      this.fetchSharePosts_a()
     },
     onPullDownRefresh() {
       this.fetchSharePosts(true);
+      this.fetchSharePosts_a(true);
     },
     mounted() {
       this.findAllNotices();
+      this.getUser();
     },
     onShow() {
       this.fetchSharePosts(true);
+      this.fetchSharePosts_a(true);
     }
   }
 </script>
@@ -140,16 +199,20 @@
   .shuoming {
     padding: 15px;
     font-size: 14px;
+
     span {
       font-weight: bold;
     }
+
     h3 {
       font-size: 16px;
       line-height: 32px;
       font-weight: bold;
     }
+
     ul {
       margin: 0 10px 0 10px;
+
       li {
       }
     }
